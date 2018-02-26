cc1853c69dca2bc511cab258292b3165c75c60a9
robert <yusheng.chen@sunplus.com>
2018-02-26 17:34:45 +0800

    [INITRAMFS] Add initramfs build for minimal Linux kernel booting [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc]
    	1. Add busybox/busybox-1.24.1.tar.bz2
    	2. To build initramfs:
    
    	   cd initramfs/
    	   ./build_disk.sh
    
    	This step will rebuild busybox and re-create disk/ as initramfs disk.
    	Default sc7021 kernel config (CONFIG_INITRAMFS_SOURCE)
    	refers to this "disk/" path.
    
    [Test   Case] Zebu/B-chip ARM9 boots to shell ok
    [Compile] OK [Coverity] N/A [Selftest] OK
    [GPL-licensed & Third-party Source] N/A

---
+++ initramfs/build_disk.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Toolchain
+export PATH="../../../build/tools/armv5-eabi--glibc--stable/bin/:$PATH"
+ARCH=arm
+CROSS=armv5-glibc-linux-
+#CROSS=armv7hf-glibc-linux-
+
+# Output
+DISKZ=disk-base-static.tgz
+#DISKZ=disk-base-dynamic.tgz
+DISKOUT=`pwd`/disk
+
+# Busybox
+BBX=busybox-1.24.1
+BBXZ=../busybox/$BBX.tar.bz2
+BBXCFG=configs/bbx_static_defconfig
+#BBXCFG=configs/bbx_dynamic_defconfig
+
+# Check sources
+if [ ! -f $DISKZ ];then
+	echo "Not found base: $DISKZ"
+fi
+
+if [ ! -f $BBXZ ];then
+	echo "Not found busybox: $BBXZ"
+	exit 1
+fi
+
+echo "Prepare disk base"
+rm -rf $DISKOUT
+tar xzf $DISKZ
+
+echo "Prepare busybox"
+rm -rf $BBX
+tar xjf $BBXZ
+cp -vf $BBXCFG $BBX/.config
+
+echo "Build busybox"
+echo make -C $BBX -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS CONFIG_PREFIX=$DISKOUT install
+make -C $BBX -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS CONFIG_PREFIX=$DISKOUT all
+
+echo "Install busybox"
+cd $BBX && make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS CONFIG_PREFIX=$DISKOUT install
+echo "Installed ($BBXCFG)"
