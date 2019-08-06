f5bb9a8e05dcdc77f18004f9f2e4d5ef74d724de
haiq.tang <haiq.tang@sunmedia.com.cn>
2019-08-05 19:38:18 -0700

    [rootfs][EMMC]set EXT2 rootfs to replace squashfs for EMMC boot [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] emmc boot use the ext2 rootfs. 		 1.use mke2fs to create ext2 rootfs 		 2.resize2fs /dev/mmcblk0p7 in rc.overlay [Test   Case] N/A [Compile] OK [Coverity] N/A [Selftest] N/A [GPL-licensed & Third-party Source] N/A

--- gen_root.sh
+++ gen_root.sh
@@ -1,5 +1,8 @@
 OUT_IMG=rootfs.img
 WORK_DIR=./initramfs/disk
+
+if [ 0 -eq 1 ];then
+##########  squash fs ############
 MKSQFS_COMPOPT="-comp lzo -Xcompression-level 9"
 MKSQFS=./tools/mksquashfs
 
@@ -13,3 +16,29 @@ if [ ! -d $WORK_DIR ];then
 fi
 
 $MKSQFS $WORK_DIR $OUT_IMG -all-root -noappend $MKSQFS_COMPOPT
+
+else
+##########  ext2 fs ############
+EXT2=./tools/mke2fs
+
+if [ "$1" != "" ];then
+	OUT_IMG=$1
+fi
+
+if [ ! -d $WORK_DIR ];then
+	echo "Error: $WORK_DIR doesn't exist!"
+	exit 1
+fi
+
+sz=`du -sb $WORK_DIR | cut -f1`
+echo "rootfs total size =$sz B"
+# 5% block reserved for superusers ,used 10% to calculation(mke2fs -m optiton)
+sz=$((sz*100/90))
+EXT2_SIZE=$((sz/1024/1024+1))
+echo "rootfs create size = $EXT2_SIZE M"
+rm -rf $OUT_IMG
+
+$EXT2 -d "$WORK_DIR" -m 5 -b 4096 $OUT_IMG $((EXT2_SIZE))M 
+
+fi
+
