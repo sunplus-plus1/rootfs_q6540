cb917629719d2d732b1be94916c09ff3b556dbdd
Wells Lu <wells.lu@sunplus.com>
2020-11-10 14:35:13 +0800

    [sp7021] Support JFFS2 SPI-NOR.

--- gen_root.sh
+++ gen_root.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # arguments: create different rootfs by boot mode
-# $1 : NAND EMMC SDCARD SPINOR (default: EMMC)
+# $1 : NAND EMMC SDCARD SPINOR NOR_JFFS2
 OUT_IMG=rootfs.img
 WORK_DIR=./initramfs/disk
 
@@ -10,7 +10,7 @@ if [ "$1" = "EMMC" ]; then
 	echo -e  "\E[1;33m ========make ext3 fs========== \E[0m"
 	EXT3=./tools/mke2fs
 
-	if [ ! -d $WORK_DIR ];then
+	if [ ! -d $WORK_DIR ]; then
 		echo "Error: $WORK_DIR doesn't exist!"
 		exit 1
 	fi
@@ -41,29 +41,29 @@ elif [ "$1" = "NAND" ]; then
 
 #mkfs.ubifs+ubi write: Can automatically set the size of the root file system by partition size in ISP,
 #                      use the ubi cmd to write rootfs into nand in ISP, need add ubi config in uboot; used it!!!
-#mkfs.ubifs+ubinize+nand write: the rootfs size is fixed in ubi.cfg that used in ubinize function. this 
+#mkfs.ubifs+ubinize+nand write: the rootfs size is fixed in ubi.cfg that used in ubinize function. this
 #                               can use the nand write cmd to write dat into nand, no need do something in uboot.
 	echo -e  "\E[1;33m ========make ubi fs========== \E[0m"
 	MKFS_UBIFS=./tools/mkfs.ubifs
 	UBINIZE=./tools/ubinize
 	UBI_CFG=./ubi.cfg
-	
-	####modify this for different nand size#### 
-	MAX_ERASE_BLK_CNT=2030    #nand size 1G:1020 2G:2030 
+
+	####modify this for different nand size####
+	MAX_ERASE_BLK_CNT=2030    #nand size 1G:1020 2G:2030
 	NAND_PAGESIZE=2048
 	NAND_BLK_PAGESIZE=64
 	NAND_LOGIC_REASE_SIZE=$(($NAND_BLK_PAGESIZE-2))*$NAND_PAGESIZE  # size = (blockcnt-2)*2048
 
-	if [ ! -d $WORK_DIR ];then
+	if [ ! -d $WORK_DIR ]; then
 		echo "Error: $WORK_DIR doesn't exist!"
 		exit 1
 	fi
-	
-	if [ ! -x $MKFS_UBIFS ];then
+
+	if [ ! -x $MKFS_UBIFS ]; then
 		echo "Error: $MKFS_UBIFS doesn't exist!"
 		exit 1
 	fi
-	$MKFS_UBIFS -r $WORK_DIR -m $NAND_PAGESIZE -e $(($NAND_LOGIC_REASE_SIZE)) -c $MAX_ERASE_BLK_CNT -F -o $OUT_IMG 
+	$MKFS_UBIFS -r $WORK_DIR -m $NAND_PAGESIZE -e $(($NAND_LOGIC_REASE_SIZE)) -c $MAX_ERASE_BLK_CNT -F -o $OUT_IMG
 
 	if false; then #mkfs.ubifs+ubinize  not used
 
@@ -74,10 +74,10 @@ elif [ "$1" = "NAND" ]; then
 			NAND_ROOTFS_SIZE=100MiB
 			echo -e  "\E[1;33m 1G nand  \E[0m"
 		elif [ 2030 -eq $MAX_ERASE_BLK_CNT ];then
-			NAND_ROOTFS_SIZE=210MiB  
+			NAND_ROOTFS_SIZE=210MiB
 			echo -e  "\E[1;33m 2G nand  \E[0m"
 		fi
-		
+
 		echo "[ubifs]" >$UBI_CFG
 		echo "mode=ubi" >>$UBI_CFG
 		echo "image=nand.img" >>$UBI_CFG
@@ -88,21 +88,26 @@ elif [ "$1" = "NAND" ]; then
 		echo "vol_size=$NAND_ROOTFS_SIZE" >>$UBI_CFG
 
 		echo -e  "\E[1;33m ========rootfs = $NAND_ROOTFS_SIZE  pagesize=$NAND_PAGESIZE========== \E[0m"
-		$MKFS_UBIFS -r $WORK_DIR -m $NAND_PAGESIZE -e $(($NAND_LOGIC_REASE_SIZE)) -c $MAX_ERASE_BLK_CNT -F -o nand.img 
+		$MKFS_UBIFS -r $WORK_DIR -m $NAND_PAGESIZE -e $(($NAND_LOGIC_REASE_SIZE)) -c $MAX_ERASE_BLK_CNT -F -o nand.img
 		$UBINIZE -v -o $OUT_IMG -m $NAND_PAGESIZE -p $(($NAND_BLK_SIZE/1024))KiB $UBI_CFG
 		rm -rf nand.img
 	fi
+elif [ "$1" = "SPINOR" ]; then
+	echo "Skip generating rootfs.img for SPINOR!"
+
+elif [ "$1" = "NOR_JFFS2" ]; then
+	echo "Skip generating rootfs.img for NOR_JFFS2!"
+
 else
 #####################################  squash fs ############################################
 	echo -e  "\E[1;33m ========make squash fs========== \E[0m"
 	MKSQFS_COMPOPT="-comp lzo -Xcompression-level 9"
 	MKSQFS=./tools/mksquashfs
 
-	if [ ! -d $WORK_DIR ];then
+	if [ ! -d $WORK_DIR ]; then
 		echo "Error: $WORK_DIR doesn't exist!"
 		exit 1
 	fi
 
 	$MKSQFS $WORK_DIR $OUT_IMG -all-root -noappend $MKSQFS_COMPOPT
 fi
-
