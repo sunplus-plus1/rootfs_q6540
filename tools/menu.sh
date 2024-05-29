73e358a74bd8cfd75e5e1e633ecc609d67bbb77d
jacky.hsieh <jacky.hsieh@sunplus.com>
2024-05-29 14:31:05 +0800

    change select rootfs order of buildroot

--- tools/menu.sh
+++ tools/menu.sh
@@ -12,8 +12,8 @@ menu_rootfs_title_num()
 
 	# <priority>:<type>:<title>:[folder]:[archive]\n
 	MENU_ROOTFS_TITLES='1:BUSYBOX:BusyBox 1.31.1::\n'
-	MENU_ROOTFS_TITLES+='999:YOCTO:Yocto::\n'
-	MENU_ROOTFS_TITLES+='1999:BUILDROOT:Buildroot::\n'
+	MENU_ROOTFS_TITLES+='1099:BUILDROOT:Buildroot::\n'
+	MENU_ROOTFS_TITLES+='1199:YOCTO:Yocto::\n'
 
 	configs=$(find $search_dirs -maxdepth 4 -type f -name 'menu.config')
 	for config in $configs
