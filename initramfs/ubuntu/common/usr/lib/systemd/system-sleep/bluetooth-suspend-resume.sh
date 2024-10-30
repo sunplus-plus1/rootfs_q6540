126f37fb1fa36390cd307b709ad4ec04f087fa2f
Akira.Yu <akira.yu@sunplus>
2024-10-30 17:37:37 +0800

    [Ubuntu] Use rfkill-gpio to control AP6256 BT_REG_ON.          This will reset AP6256 Bluetooth after system suspend and resume.          Then Bluetooth will be workable after system re-load firmware.

--- initramfs/ubuntu/common/usr/lib/systemd/system-sleep/bluetooth-suspend-resume.sh
+++ initramfs/ubuntu/common/usr/lib/systemd/system-sleep/bluetooth-suspend-resume.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
+BLUETOOTH0_LINK=/sys/class/bluetooth/hci0
+BLUETOOTH_SERVICE=/etc/systemd/system/hciuart.service
 #pre-->system suspend, post-->system resume
-#GPIO57-->AP6256 BT_REG_ON: It needs to be set low to high after system suspend and before reload FW bin file.
-#                           bluetooth.service will call hciuart.service to reload FW bin file. 
+#In EV & IO board, GPIO57-->AP6256 BT_REG_ON: It needs to be set low to high after system suspend and before reload FW bin file.
+#Set GPIO57 to be rfkill_bluetooth GPIO control source, then BT_REG_ON will be switched during "rfkill block bluetooth" and "rfkill unblock bluetooth".
+#bluetooth.service will call hciuart.service to reload FW bin file.
 case $1 in
     pre)
-        if [ -L /sys/class/bluetooth/hci0 ]; then
+        if [ -L $BLUETOOTH0_LINK ]; then
             rfkill block bluetooth
         fi
         ;;
     post)
-        if [ -L /sys/class/bluetooth/hci0 ]; then
-            echo 57 > /sys/class/gpio/export
-            echo 0 > /sys/class/gpio/GPIO57/value
-            sleep 0.1
-            echo 1 > /sys/class/gpio/GPIO57/value
-            sleep 0.2
-            systemctl restart bluetooth.service
-            sleep 0.5
+        if [ -L $BLUETOOTH0_LINK ] && [ -f $BLUETOOTH_SERVICE ]; then
             rfkill unblock bluetooth
-            sleep 0.1
+            systemctl restart bluetooth.service
+        elif [ -L $BLUETOOTH0_LINK ];then
+            rfkill unblock bluetooth
         fi
         ;;
 esac
